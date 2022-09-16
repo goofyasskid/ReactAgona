1e314562b8bd25e7e3de6c99d490f27daebfbfb8
goofyasskid <sergio.shorin@mail.ru>
2022-09-16 22:42:16 +0300

    start

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.sass';
+
+const App = () =>{
+  return (
+    <div className="App">
+      <header className="App-header">
+        lslsl
+      </header>
+    </div>
+  );
+}
+
+export default App;

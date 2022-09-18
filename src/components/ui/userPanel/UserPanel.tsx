65505f72e944e13013e7fbb98eaaa2655cbb5569
goofyasskid <sergio.shorin@mail.ru>
2022-09-18 08:12:55 +0300

    header finished

--- src/components/ui/userPanel/UserPanel.tsx
+++ src/components/ui/userPanel/UserPanel.tsx
@@ -1,10 +1,27 @@
-import style from './UserPanel.module.sass'
-type Props = {}
+import avatar from "../../../assets/avatar.svg";
+import money from "../../../assets/money.svg";
+import point from "../../../assets/point.svg";
+import style from "./UserPanel.module.sass";
 
-const UserPanel = (props: Props) => {
+const UserPanel = () => {
   return (
-    <div>UserPanel</div>
-  )
-}
+    <div className={style.user_panel}>
+      <ul>
+        <li className={style.user_panel__point}>
+          <img src={point} alt="point" />
+          <p>Казань</p>
+        </li>
+        <li>
+          <img src={money} alt="money" />
+          <p>1000</p>
+        </li>
+        <li>
+          <img src={avatar} alt="avatar" />
+          <p>Алексей</p>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-export default UserPanel
+export default UserPanel;
